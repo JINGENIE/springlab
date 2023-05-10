7f4ad1c9aaf25a0888bd944360c34f6f927d8aa7
JINGENIE <geniejin96@gmail.com>
2023-05-10 09:10:33 +0900

    web15

---
+++ src/main/java/com/kbstar/Web15Application.java
@@ -0,0 +1,22 @@
+package com.kbstar;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+@Slf4j
+@SpringBootApplication
+public class Web15Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Web15Application.class, args);
+    }
+    @Bean
+    public InternalResourceViewResolver setupViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
